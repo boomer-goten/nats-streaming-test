002360087f8f58801d22a032584b732b31fd295d
alexey <alexey>
2024-01-29 13:57:00 +0300

    test

--- publish_data/pub/main.go
+++ publish_data/pub/main.go
@@ -1,115 +1,115 @@
-package main
+// package main
 
-import (
-	"bufio"
-	"encoding/json"
-	"fmt"
-	"log"
-	"math/rand"
-	"os"
-	"reflect"
-	"time"
+// import (
+// 	"bufio"
+// 	"encoding/json"
+// 	"fmt"
+// 	"log"
+// 	"math/rand"
+// 	"os"
+// 	"reflect"
+// 	"time"
 
-	"github.com/nats-io/stan.go"
-)
+// 	"github.com/nats-io/stan.go"
+// )
 
-const (
-	cluster_id = "test-cluster"
-	client_id  = "wb_publisher"
-	channel    = "flow"
-)
+// const (
+// 	cluster_id = "test-cluster"
+// 	client_id  = "wb_publisher"
+// 	channel    = "flow"
+// )
 
-func main() {
-	sc, err := stan.Connect(cluster_id, client_id)
-	if err != nil {
-		log.Fatal(err)
-	}
-	scanner := bufio.NewScanner(os.Stdin)
-	defer sc.Close()
-	rand.Seed(time.Now().UnixNano())
-	for {
-		fmt.Printf("%s\n", "Выберите операцию")
-		fmt.Printf("%s\n", "1. Сгенерировать валидное json сообщение и отправить в stan")
-		fmt.Printf("%s\n", "2. Сгенерировать невалидное json сообщение и отправить в stan")
-		fmt.Printf("%s\n", "3. Прекратить посылать сообщения и выйти из программы")
-		scanner.Scan()
-		value := scanner.Text()
-		switch {
-		case value == "1":
-			SendMessageToStan(GenerateTrueJson(), sc)
-		case value == "2":
-			SendMessageToStan(GenerateFalseJson(), sc)
-		case value == "3":
-			os.Exit(0)
-		default:
-			fmt.Printf("%s", "Введите корректное значение")
-			continue
-		}
-	}
-}
+// func main() {
+// 	sc, err := stan.Connect(cluster_id, client_id)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	scanner := bufio.NewScanner(os.Stdin)
+// 	defer sc.Close()
+// 	rand.Seed(time.Now().UnixNano())
+// 	for {
+// 		fmt.Printf("%s\n", "Выберите операцию")
+// 		fmt.Printf("%s\n", "1. Сгенерировать валидное json сообщение и отправить в stan")
+// 		fmt.Printf("%s\n", "2. Сгенерировать невалидное json сообщение и отправить в stan")
+// 		fmt.Printf("%s\n", "3. Прекратить посылать сообщения и выйти из программы")
+// 		scanner.Scan()
+// 		value := scanner.Text()
+// 		switch {
+// 		case value == "1":
+// 			SendMessageToStan(GenerateTrueJson(), sc)
+// 		case value == "2":
+// 			SendMessageToStan(GenerateFalseJson(), sc)
+// 		case value == "3":
+// 			os.Exit(0)
+// 		default:
+// 			fmt.Printf("%s", "Введите корректное значение")
+// 			continue
+// 		}
+// 	}
+// }
 
-func GenerateTrueJson() []byte {
-	var ModelData TrueJSONFile
-	valueOrd := reflect.ValueOf(&ModelData.Ord).Elem()
-	valuePay := reflect.ValueOf(&ModelData.Ord.Payment).Elem()
-	valueDel := reflect.ValueOf(&ModelData.Ord.Delivery).Elem()
-	FillStructureRandomValue(&valueOrd)
-	FillStructureRandomValue(&valuePay)
-	FillStructureRandomValue(&valueDel)
-	size_items := generateRandomInt(1, 3)
-	for i := 0; i < size_items; i++ {
-		var item Item
-		valueIt := reflect.ValueOf(&item).Elem()
-		FillStructureRandomValue(&valueIt)
-		ModelData.Ord.Items = append(ModelData.Ord.Items, item)
-	}
-	data, _ := json.Marshal(ModelData)
-	fmt.Printf("%s\n", data)
-	return data
-}
+// func GenerateTrueJson() []byte {
+// 	var ModelData TrueJSONFile
+// 	valueOrd := reflect.ValueOf(&ModelData.Ord).Elem()
+// 	valuePay := reflect.ValueOf(&ModelData.Ord.Payment).Elem()
+// 	valueDel := reflect.ValueOf(&ModelData.Ord.Delivery).Elem()
+// 	FillStructureRandomValue(&valueOrd)
+// 	FillStructureRandomValue(&valuePay)
+// 	FillStructureRandomValue(&valueDel)
+// 	size_items := generateRandomInt(1, 3)
+// 	for i := 0; i < size_items; i++ {
+// 		var item Item
+// 		valueIt := reflect.ValueOf(&item).Elem()
+// 		FillStructureRandomValue(&valueIt)
+// 		ModelData.Ord.Items = append(ModelData.Ord.Items, item)
+// 	}
+// 	data, _ := json.Marshal(ModelData)
+// 	fmt.Printf("%s\n", data)
+// 	return data
+// }
 
-func GenerateFalseJson() []byte {
-	var ModelData FalseJSONFile
-	valueFalse := reflect.ValueOf(&ModelData).Elem()
-	valuePay := reflect.ValueOf(&ModelData.Pay).Elem()
-	FillStructureRandomValue(&valuePay)
-	FillStructureRandomValue(&valueFalse)
-	data, _ := json.Marshal(ModelData)
-	fmt.Printf("%s\n", data)
-	return data
-}
+// func GenerateFalseJson() []byte {
+// 	var ModelData FalseJSONFile
+// 	valueFalse := reflect.ValueOf(&ModelData).Elem()
+// 	valuePay := reflect.ValueOf(&ModelData.Pay).Elem()
+// 	FillStructureRandomValue(&valuePay)
+// 	FillStructureRandomValue(&valueFalse)
+// 	data, _ := json.Marshal(ModelData)
+// 	fmt.Printf("%s\n", data)
+// 	return data
+// }
 
-func SendMessageToStan(data []byte, stanConnection stan.Conn) error {
-	return stanConnection.Publish(channel, data)
-}
+// func SendMessageToStan(data []byte, stanConnection stan.Conn) error {
+// 	return stanConnection.Publish(channel, data)
+// }
 
-func generateRandomString() string {
-	charSet := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	b := make([]byte, 10)
-	for i := range b {
-		b[i] = charSet[rand.Intn(len(charSet))]
-	}
-	return string(b)
-}
+// func generateRandomString() string {
+// 	charSet := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+// 	b := make([]byte, 10)
+// 	for i := range b {
+// 		b[i] = charSet[rand.Intn(len(charSet))]
+// 	}
+// 	return string(b)
+// }
 
-func generateRandomInt(min, max int) int {
-	return rand.Intn(max-min+1) + min
-}
+// func generateRandomInt(min, max int) int {
+// 	return rand.Intn(max-min+1) + min
+// }
 
-func generateRandomFloat() float32 {
-	return float32(rand.NormFloat64())
-}
+// func generateRandomFloat() float32 {
+// 	return float32(rand.NormFloat64())
+// }
 
-func FillStructureRandomValue(refl *reflect.Value) {
-	for i := 0; i < refl.NumField(); i++ {
-		fieldValue := refl.Field(i)
-		switch fieldValue.Kind() {
-		case reflect.String:
-			fieldValue.SetString(generateRandomString())
-		case reflect.Int:
-			fieldValue.SetInt(int64(generateRandomInt(0, 100)))
-		case reflect.Float32:
-			fieldValue.SetFloat(float64(generateRandomFloat()))
-		}
-	}
-}
+// func FillStructureRandomValue(refl *reflect.Value) {
+// 	for i := 0; i < refl.NumField(); i++ {
+// 		fieldValue := refl.Field(i)
+// 		switch fieldValue.Kind() {
+// 		case reflect.String:
+// 			fieldValue.SetString(generateRandomString())
+// 		case reflect.Int:
+// 			fieldValue.SetInt(int64(generateRandomInt(0, 100)))
+// 		case reflect.Float32:
+// 			fieldValue.SetFloat(float64(generateRandomFloat()))
+// 		}
+// 	}
+// }
